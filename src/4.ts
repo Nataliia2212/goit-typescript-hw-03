8e03cf7d9d3c4cade947db4d2ca6d46b9c9872b0
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-03-17 23:20:26 +0200

    1-4

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,53 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number) {}
 
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: object) {}
+  getKey(): object {
+    console.log(this.key);
+    return this.key;
+  }
+}
+
+abstract class House {
+  private tenants: Person[] = [];
+  protected door: boolean = false;
+  constructor(key: object) {}
+
+  comeIn(key: object) {
+    if (this.door) {
+      const tenant = new Person(key);
+      this.tenants.push(tenant);
+      console.log(this.tenants);
+    }
+  }
+
+  public abstract OpenDoor(personKey: object): boolean;
+}
+
+class MyHouse extends House {
+  constructor(key: object) {
+    super(key);
+  }
+  public OpenDoor(personKey: object): boolean {
+    if (personKey === key) {
+      return true;
+    }
+    return false;
+  }
+}
+
+const key = new Key(Math.random());
 const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
+house.OpenDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
